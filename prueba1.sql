5392504446919fa3468259b79fa94429318e2fb8
desarrollomovilbancomonex <desarrollomovilbancomonex@192.168.99.1>
2019-02-25 16:47:01 -0600

    subo db test

---
+++ prueba1.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXIST prueba1;
+
+BEGIN;
+
+CREATE TABLE prueba1(id serial PRIMARY KEY,string varchar);
+INSERT INTO prueba1('Valor 1'); 
+INSERT INTO prueba1('Valor 2'); 
+INSERT INTO prueba1('Valor 3'); 
+INSERT INTO prueba1('Valor 4'); 
