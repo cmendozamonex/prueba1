c69afd7c5b27e104f6ed496dd2efcd7029bc4eda
desarrollomovilbancomonex <desarrollomovilbancomonex@192.168.99.1>
2019-02-25 17:31:15 -0600

    test commit

--- prueba1.sql
+++ prueba1.sql
@@ -7,3 +7,5 @@ INSERT INTO prueba1(string) VALUES('Valor 1');
 INSERT INTO prueba1(string) VALUES('Valor 2'); 
 INSERT INTO prueba1(string) VALUES('Valor 3'); 
 INSERT INTO prueba1(string) VALUES('Valor 4'); 
+
+COMMIT;
